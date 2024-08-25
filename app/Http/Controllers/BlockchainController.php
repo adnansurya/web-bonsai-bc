90397c1ea4b8bc3c6fddb05adee9772faa03a1c9
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2024-08-25 21:26:21 +0700

    Update BlockchainController.php
    edit controller

--- app/Http/Controllers/BlockchainController.php
+++ app/Http/Controllers/BlockchainController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Blockchain;
 
 class BlockchainController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        // Validasi parameter
+        $request->validate([
+            'hash' => 'required|string',
+            'type' => 'required|string',
+        ]);
+
+        // Simpan data ke dalam tabel blockchains
+        $blockchain = new Blockchain();
+        $blockchain->product_id = 1; // Anda dapat mengganti dengan nilai yang sesuai
+        $blockchain->transaction_hash = $request->hash;
+        $blockchain->data_type = $request->type;
+        $blockchain->save();
+
+        return response()->json(['success' => true, 'message' => 'Data blockchain berhasil ditambahkan']);
+    }
 }
