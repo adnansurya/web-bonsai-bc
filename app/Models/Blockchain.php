c2fba9d37753f8751a570feabd07affedb3e45cf
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2024-08-25 21:25:30 +0700

    Update Blockchain.php
    edit model Blockchain

--- app/Models/Blockchain.php
+++ app/Models/Blockchain.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Blockchain extends Model
 {
-    use HasFactory;
+    // Tentukan nama tabel jika berbeda dari nama model yang di-plural-kan
+    protected $table = 'blockchains';
+
+    // Tentukan kolom yang bisa diisi massal
+    protected $fillable = [
+        'product_id',
+        'transaction_hash',
+        'data_type',
+    ];
 }
